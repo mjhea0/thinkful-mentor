4537442b59ce686d93c9be77e5dbc95c25c0a842
Michael Herman <hermanmu@gmail.com>
2013-12-23 21:51:40 -0600

    added another calc

---
+++ tip-calc/tip-calc-refactor-new.py
@@ -0,0 +1,10 @@
+# refactor me!
+
+raw_bill= float(raw_input("Bill Total Amount: "))
+tip_amt= raw_bill * .15
+total_bill= raw_bill + tip_amt
+print "Your tip is", tip_amt, "dollars."
+print "Your total bill, including the tip of", tip_amt, " dollars, is", total_bill, "dollars."
+raw_input("Press enter key to exit")
+
+
