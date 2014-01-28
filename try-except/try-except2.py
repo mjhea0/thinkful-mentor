b1453a1b8cac69bc53301808f243af83ebcc2522
Michael Herman <hermanmu@gmail.com>
2014-01-27 22:04:42 -0700

    functions

--- try-except/try-except2.py
+++ try-except/try-except2.py
@@ -1,28 +1,30 @@
 # try/except example
 
-def assert_positive(num_list):
+def assert_positive(num):
     """assert that each number is greater than 0"""
-    for num in num_list:
-        try:
-            assert num > 0
-        except AssertionError:
-            print "Sorry. " + str(num) + " is less than zero. Try again"
-            return 0
-        else:
-            return 1
+    try:
+        assert num > 0
+    except AssertionError:
+        print "Sorry. " + str(num) + " is less than zero. Try again"
+        return 1
+    else: 
+        return 0
 
 
 def calculate_total(num1,num2,num3):
     """calculate total from three numbers"""
     num_list = []
+    test = []
     num_list.extend((num1, num2, num3))
-    if assert_positive(num_list) == 1:
-        total = num1 + num2 + num3
-        print "The total is: " + str(total)
+    for num in num_list:
+        test.append(assert_positive(num))
+        if len(test) == 3 and sum(test) == 0:
+            total = sum(num_list)
+            print "The total is: " + str(total)
 
 
 ### --- run code --- ###
 
 calculate_total(1,2,-3)
 calculate_total(1,2,3)
-calculate_total(-10,2,3)
+calculate_total(1,-2,3)
