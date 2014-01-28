8845bfc05025a7a374415fa2dd29884528f61bae
Michael Herman <hermanmu@gmail.com>
2014-01-27 22:42:24 -0700

    updates

--- try-except/try-except3.py
+++ try-except/try-except3.py
@@ -4,12 +4,8 @@ def assert_positive(num):
     """assert that each number is greater than 0"""
     try:
         assert num > 0
-        float(num)
     except AssertionError:
-        print "Sorry. " + str(num) + " is less than zero. Try again."
-        return 1
-    except ValueError:
-        print "Sorry. " + str(num) + " is not a number. Try again."
+        print "Sorry. " + str(num) + " is less than zero. Try again"
         return 1
     else: 
         return 0
@@ -31,5 +27,4 @@ def calculate_total(num1,num2,num3):
 
 calculate_total(1,2,-3)
 calculate_total(1,2,3)
-calculate_total(1,-20,3)
-calculate_total(1,-2,"test")
+calculate_total(1,-2,3)
