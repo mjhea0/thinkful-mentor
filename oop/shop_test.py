67d2428121e86987d505fcdea6fc672db2e22b5f
Michael Herman <hermanmu@gmail.com>
2014-02-12 06:31:36 -0700

    updates

--- oop/shop_test.py
+++ oop/shop_test.py
@@ -7,16 +7,22 @@ class TestShoppingCart(unittest.TestCase):
     def setUp(self):
         self.go_shopping = ShoppingCart('Mike')
         self.banana = Fruit('banana', 1)
+        self.cucumber = Vegetable('cucumber', 3)
 
     def test_get_total_items(self):
-        pass
+        self.go_shopping.add_item(self.banana.item, self.banana.item_price)
+        self.go_shopping.add_item(self.cucumber.item, self.cucumber.item_price)
+        self.assertEqual(len(self.go_shopping.items_in_cart),2)
 
     def test_add_item_to_cart(self):
         self.go_shopping.add_item(self.banana.item, self.banana.item_price)
         self.assertEqual(self.go_shopping.items_in_cart, {'banana':1})
 
     def test_empty_shopping_cart(self):
-        pass
+        self.go_shopping.add_item(self.banana.item, self.banana.item_price)
+        self.go_shopping.add_item(self.cucumber.item, self.cucumber.item_price)
+        self.go_shopping.empty_cart()
+        self.assertEqual(len(self.go_shopping.items_in_cart),0)
 
 if __name__ == '__main__':
     unittest.main()
