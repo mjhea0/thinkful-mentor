a164f8b905bf37442504078f95bf5ab184f39bfd
Michael Herman <hermanmu@gmail.com>
2014-02-14 10:05:27 -0800

    updates

---
+++ recurrsion/factorial_recurrsion.py
@@ -0,0 +1,44 @@
+# recursive
+
+def fact(x):
+    if x == 0:
+        return 1
+    else:
+        return x * fact(x-1)
+
+def fact_loop(x):
+    f = 1
+    while (x > 0):
+        f = f * x
+        x = x - 1
+    return f
+
+#### ---- run code ---- ####
+
+print fact(3) == 6
+print fact(5) == 120
+print fact(1) == 1
+
+print fact_loop(3) == 6
+print fact_loop(5) == 120
+print fact_loop(1) == 1
+
+
+#### --- decorator ---- ####
+
+import time
+
+def timing_function(some_function):
+    def wrapper():
+        t1 = time.time()
+        some_function()
+        t2 = time.time()
+        return "Time it took to run the function: " + str((t2-t1)) + "\n"
+    return wrapper
+
+@timing_function
+def my_function():
+	pass
+
+
+print my_function()
