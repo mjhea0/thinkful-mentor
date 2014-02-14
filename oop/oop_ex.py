f377c0216ad38e0e76e42c4c89b68edb1157f179
Michael Herman <hermanmu@gmail.com>
2014-02-14 11:04:23 -0800

    updates

---
+++ oop/oop_ex.py
@@ -0,0 +1,27 @@
+class Dog(object):
+
+    # Initializer / Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # instance method
+    def description(self):
+        return self.name, self.age
+
+    # instance method
+    def speak(self, sound):
+        return "%s says %s" % (self.name, sound)
+
+    def moves(self, verb):
+        return "{} is {}".format(self.name,verb)
+
+# Instantiate the Dog object
+mikey = Dog("Mikey", 6)
+
+# call our instance method
+print mikey.description()
+print mikey.speak("Gruff Gruff")
+
+andrew = Dog("Andrew",15)
+print andrew.moves("walking")
--- recursion/factorial_recursion.py
+++ recursion/factorial_recursion.py
@@ -26,19 +26,44 @@ print fact_loop(1) == 1
 
 #### --- decorator ---- ####
 
-import time
+import time, random
 
 def timing_function(some_function):
-    def wrapper():
+    def wrapper(num):
         t1 = time.time()
-        some_function()
+        some_function(num)
         t2 = time.time()
         return "Time it took to run the function: " + str((t2-t1)) + "\n"
     return wrapper
 
+
 @timing_function
-def my_function():
-	pass
+def fact(x):
+    if x == 0:
+        return 1
+    else:
+        return x * fact(x-1)
 
 
-print my_function()
+@timing_function
+def fact_loop(x):
+    f = 1
+    while (x > 0):
+        f = f * x
+        x = x - 1
+    return f
+
+print fact(5)
+print fact_loop(5)
+
+# @timing_function
+# def my_function():
+#     new_list = []
+#     i = 0
+#     while i < 10000:
+#         for x in xrange(0,100):
+#             new_list.append(x+random.randint(0,10))
+#         i += 1
+
+# print my_function()
+
