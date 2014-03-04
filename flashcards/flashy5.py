2b942442c32cbd01e014dbc0d0fb029e7203ba34
Michael Herman <hermanmu@gmail.com>
2014-03-04 14:53:43 -0600

    updates

---
+++ flashcards/flashy5.py
@@ -0,0 +1,121 @@
+# add functions
+
+"""
+to do:
+
+1. oop
+2. tests
+3. questions right vs questions wrong
+4. timer
+5. display questions at end with their right answer
+6. make sure you don't repeat options (recursive function)
+
+"""
+
+import os, random, sys
+
+class FlashCards(object):
+
+    def __init__(self):
+        pass
+
+    def cls(self):
+        """ clears the screen """
+        os.system(['clear','cls'][os.name == 'nt'])
+
+
+    def read_files(self):
+
+        """ 
+        opens the question and answer files
+        then parses them line by line
+        """
+
+        content_list = []
+
+        file1 = open('questions.txt', 'r')
+        file2 = open('answers.txt', 'r')
+
+        file1_content = file1.readlines()
+        file2_content = file2.readlines()
+
+        content_list.extend([file1_content, file2_content])
+        return content_list
+
+
+
+    def display_question_answers(self,content):
+        
+        """
+        displays question, list of potential answers (options)
+        returns question, options, and user answer in a list
+        """
+
+        
+
+        question = random.randint(0, len(content[0])-1)
+        options = [random.randint(0, len(content[1])-1),
+        random.randint(0, len(content[1])-1),
+        random.randint(0, len(content[1])-1)]
+        options[random.randint(0,2)] = question
+        output_list=[]
+        output_list.extend([options,question, content[0][question], content[1][options[0]],
+                             content[1][options[1]], content[1][options[2]]])
+        return output_list
+
+    
+    def user_answer(self):
+
+
+        answer = input('\nYour choice: ')
+        answer_list = []
+        answer_list.extend([answer])
+        return answer_list
+
+    def answer_question(self, options_question_answer_list, answer):
+
+        if options_question_answer_list[0][answer[0]-1] == options_question_answer_list[1]:
+            return "Correct!"
+        else:   
+            return "Wrong"
+
+
+    def play_again(self):
+        again = raw_input('Press Enter to continue (or x then Enter to exit) ...')
+        return again
+
+
+
+    def calculate_score(self, score, results):
+
+        """calculates score"""
+        if results == "Correct!":
+            score += 1
+        print "your score is {}".format(score)
+        return score
+        
+
+
+if __name__ == '__main__':
+
+    again = 0
+    count = 0
+    score = 0
+    create = FlashCards()
+    while True:
+        if again == "x":
+            sys.exit() 
+        else:
+            create.cls()
+            data = create.read_files()
+            display = create.display_question_answers(data)
+            print 'Question: ' + display[2]
+            print '1: ' + display[3]
+            print '2: ' + display[4]
+            print '3: ' + display[5]
+            results = create.user_answer()
+            correct = create.answer_question(display,results)
+            print correct
+            score = create.calculate_score (score, correct)
+            again = create.play_again ()
+            count += 1
