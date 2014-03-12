11aaccaa0fdfa1925aac32b490cf7d5e024313c7
Michael Herman <hermanmu@gmail.com>
2014-03-11 18:20:49 -0600

    updates

---
+++ pollProject/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.choice_text
---
+++ pollProject/readme.md
@@ -0,0 +1,9 @@
+## My APP
+
+- Project: pollProject
+- App: polls
+
+## Django APP
+
+- Project: mysite
+- App: polls
