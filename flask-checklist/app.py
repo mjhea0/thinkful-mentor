bc5affd763a0fd1d366308b7f6af1cd3bbabe84f
Michael Herman <hermanmu@gmail.com>
2014-03-21 11:07:45 -0600

    updates

--- flask-checklist/app.py
+++ flask-checklist/app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+import sqlite3
+
+DATABASE = 'checklist.db'
+SECRET_KEY = 'hard_to_guess'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+   
  
-app = Flask(__name__)      
- 
-@app.route('/', methods=['GET', 'POST'])
 def home():
-    my_items = {"apples":13.45,"more apples":21.99}
-    new_item = {}
-    if request.method == 'POST':
-        item = request.form["item"]
-        amount = request.form["amount"]
-        my_items[item] = amount
-        return render_template('home.html', my_items=my_items)
+    g.db = connect_db()
+    cur = g.db.execute('select * from items')
+    my_items = [dict(item=row[0], amount=row[1]) for row in cur.fetchall()]  
+    g.db.close()
+    return render_template('home.html', my_items=my_items)
+
+@app.route('/add', methods=['POST'])
+def add():
+    item = request.form['item']
+    amount = request.form['post']
+    if not item or not amount:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('home'))   
     else:
-        return render_template('home.html', my_items=my_items)
+        g.db = connect_db()
+        g.db.execute('insert into items (item, amount) values (?, ?)',
+         [request.form['item'], request.form['amount']])                 
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
+
+
+
+
+
