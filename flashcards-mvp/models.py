ccecf6ede0e3ef178a9cef15f80e99e5999d4fea
Michael Herman <hermanmu@gmail.com>
2014-03-31 01:05:13 -0600

    updates

---
+++ flashcards-mvp/models.py
@@ -0,0 +1,30 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from app import db
+
+class Question(db.Model):
+
+    __tablename__ = "questions"
+
+    question_id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+
+    def __init__(self, description):
+        self.description = description
+
+    def __repr__(self):
+        return self.description
+
+class Answer(db.Model):
+
+    __tablename__ = "answers"
+
+    answer_id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    question_id = db.Column(db.Integer, ForeignKey('questions.question_id'))
+
+    def __init__(self, description):
+        self.description = description
+
+    def __repr__(self):
+        return self.description
