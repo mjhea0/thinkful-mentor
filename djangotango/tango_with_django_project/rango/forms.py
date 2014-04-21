b6cb5def19bb295fae4dc03580c98f4a062005b7
Michael Herman <hermanmu@gmail.com>
2014-04-20 18:45:27 -0600

    updated tangodjango

--- djangotango/tango_with_django_project/rango/forms.py
+++ djangotango/tango_with_django_project/rango/forms.py
@@ -29,13 +29,19 @@ class PageForm(forms.ModelForm):
         fields = ('title', 'url', 'views')
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter your email.")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
 class UserProfileForm(forms.ModelForm):
+
+    website = forms.URLField(help_text="Please enter your website.", required=False)
+    picture = forms.ImageField(help_text="Select a profile image to upload.", required=False)
+
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
