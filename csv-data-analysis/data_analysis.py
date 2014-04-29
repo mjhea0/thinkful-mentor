4cd0eede638d3809a9d3e89c5f0f8119e4c3b9b6
Michael Herman <hermanmu@gmail.com>
2014-04-29 14:14:58 -0500

    updates

--- csv-data-analysis/data_analysis.py
+++ csv-data-analysis/data_analysis.py
@@ -8,18 +8,28 @@ my_file = 'us_arrests.csv'
 
 
 def import_data(delimited_file):
+    """
+    imports the a delimited file and casts the data to a list
+    """
     with open(delimited_file, 'rb') as csvfile:
         all_data = list(csv.reader(csvfile, delimiter=','))
     return all_data
 
 
 def seperate_headings_from_data(data):
+    """
+    seperates the headings from the data
+    """
     headings = data[0]
     data.pop(0)
     print pandas.DataFrame(data, columns=headings)
 
 
 def get_basic_statistics(data):
+    """
+    grabs the values for a specific crime
+    *function should be renamed*
+    """
     murder = []
     for crime in data:
         murder.append(float(crime[1]))
@@ -27,14 +37,23 @@ def get_basic_statistics(data):
 
 
 def calculate_statistics(crime):
+    """
+    calculates mean, media, and standard Deviation
+    """
     return numpy.mean(crime), numpy.median(crime), numpy.std(crime)
 
 
 def calculate_min_and_max(crime):
+    """
+    calculates min and max values
+    """
     return numpy.min(crime), numpy.max(crime)
 
 
 def get_state(crime, min_max, data):
+    """
+    associates the state with the min and max values
+    """
     state = []
     min_index = crime.index(min_max[0])
     max_index = crime.index(min_max[1])
@@ -44,11 +63,17 @@ def get_state(crime, min_max, data):
 
 
 def create_frequency_distribution(crime):
+    """
+    creates a frequency distribution chart
+    """
     hist, bin_edges = numpy.histogram(crime, bins=10)
     return hist, bin_edges
 
 
 def create_histogram(crime):
+    """
+    creates a histogram in matplotlib
+    """
     plt.hist(crime, facecolor='green', label='murders')
     plt.title("Murder Rate Histogram")
     plt.xlabel("murder rates")
