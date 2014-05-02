42487a1d6e51f6c35cffab14ffd2e359e2b93c16
Michael Herman <hermanmu@gmail.com>
2014-05-01 20:25:35 -0500

    flask-wtf errors

---
+++ flask-form-validation/app.py
@@ -0,0 +1,46 @@
+from random import randrange
+from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import TextField, RadioField
+from wtforms.validators import Required
+
+# config
+SECRET_KEY = 'change-me-please'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+# form
+class QuestionForm(Form):
+    question_one = RadioField(
+        'Please answer yes',
+        validators=[Required("Please answer the question.")],
+        choices=[('yes', 'Yes'), ('no', 'No')])
+
+
+# views
+def error_list():
+    errors = ["first error", "second error", "third error", "fourth error"]
+    return errors
+
+
+@app.route('/', methods=['post', 'get'])
+def form():
+    form = QuestionForm()
+    answer = ""
+    errors = ""
+    if form.validate_on_submit():
+        if form.data['question_one'] == 'yes':
+            answer = "Thanks for submitting {}".format(
+                form.data['question_one'])
+        else:
+            answer = error_list()[randrange(0, len(error_list()))]
+    else:
+        errors = form.errors
+    return render_template(
+        'form.html', form=form, answer=answer, errors=errors)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
