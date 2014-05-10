8530bb195f14a4841f94c89a22b3f94e8698243d
Michael Herman <hermanmu@gmail.com>
2014-05-10 16:52:13 -0500

    jquery form

--- angular-curriculum/adder-subtractor/js/main.js
+++ angular-curriculum/adder-subtractor/js/main.js
@@ -22,7 +22,7 @@ $(function() {
 
   // validate values
   function validateValues(operator, value1, value2) {
-    if ((is_valid = !/^[0-9]+$/.test(value1)) || (is_valid = !/^[0-9]+$/.test(value2))) { 
+    if ((is_valid = !/^[0-9]+(\.[0-9]+)?$/.test(value1)) || (is_valid = !/^[0-9]+(\.[0-9]+)?$/.test(value2))) { 
       $('#alert').html('<span class="alert alert-danger">You must supply valid integers!</span>'); 
     } else {
       $('#alert').html('');
@@ -35,37 +35,37 @@ $(function() {
 
   // add
   function addValues(value1, value2) {
-    value3 = parseInt(value1) + parseInt(value2)
+    value3 = parseFloat(value1) + parseFloat(value2)
     displayAnswer(operator="plus", value3, value1, value2)
   };
 
   // subtract
   function subtractValues(value1, value2) {
-    value3 = parseInt(value1) - parseInt(value2)
+    value3 = parseFloat(value1) - parseFloat(value2)
     displayAnswer(operator="minus", value3, value1, value2)
   };
 
   // multiply
   function multiplyValues(value1, value2) {
-    value3 = parseInt(value1) * parseInt(value2)
+    value3 = parseFloat(value1) * parseFloat(value2)
     displayAnswer(operator="times", value3, value1, value2)
   };
 
   // divide
   function divideValues(value1, value2) {
-    if (parseInt(value2) === 0) { 
+    if (parseFloat(value2) === 0) { 
       $('#alert').html('<span class="alert alert-danger">You cannot divide by 0!</span>');
       console.log(typeof(value2))
     } else {
-    value3 = parseInt(value1) / parseInt(value2)
-    value3 = (Math.round(value3 * 100)/100).toFixed(2);
+    value3 = parseFloat(value1) / parseFloat(value2)
     displayAnswer(operator="divided by", value3, value1, value2)
     };
   };
 
   // display answer
   function displayAnswer(operator, answer, value1, value2) {
-    $('#answer').html('<span>'+value1+' '+operator+' '+value2+' '+'= '+answer+'<span>');
+    $('#answer').html('<span>'+value1+' '+operator+' '+value2+' '+'= '+
+      (Math.round(value3 * 100)/100).toFixed(2)+'<span>');
     $('#answer').show()
   };
 
