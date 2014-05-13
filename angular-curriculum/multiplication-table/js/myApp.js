a826dd1e9631ecc9e50ab85d9d7ad725ff865fcd
Michael Herman <hermanmu@gmail.com>
2014-05-13 10:09:47 -0700

    multiplication table v4

--- angular-curriculum/multiplication-table/js/myApp.js
+++ angular-curriculum/multiplication-table/js/myApp.js
@@ -1,32 +1,43 @@
 angular.module('myApp', [])
-    .controller('MultiplicationCtrl', function($scope, $attrs) {
-      function populateNumbers(x) {
-        var numbers = [];
-        for(var i=0; i<x; i++) {
-          numbers[i] = i + 1; 
-        };
+  .controller('DisplayCtrl', function($scope) {
+    $scope.$on('displayData', function(event, data) {
+      $scope.content = data;
+    });
+  })
+
+  .controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
+    function populateNumbers(x) {
+      var numbers = [];
+      for(var i=0; i<x; i++) 
+        {numbers[i] = i + 1; };
         return numbers;
-      }
+    }
 
     $scope.compute = function(a,b) {
-        return a * b;
-    }
-    $scope.$watch('numberLimit', function(limit) {
-        $scope.numbers = populateNumbers(limit);
+       return a * b;
+    };
+
+    $scope.$watch("numberLimit", function(limit) {
+      $scope.numbers = populateNumbers(limit);
     });
 
     $scope.numberLimit = $attrs.initialNumberLimit || 10;
-
+    
     var activeFactorA, activeFactorB;
     $scope.setActiveFactors = function(a, b) {
-        activeFactorA = a;
-        activeFactorB = b;
+      activeFactorA = a;
+      activeFactorB = b;
     };
 
-    $scope.matchesFactor = function (a, b) {
-        return a === activeFactorA || b === activeFactorB;
+    $scope.matchesFactor = function(a, b) {
+      return a === activeFactorA || b === activeFactorB;
     };
     
+    $scope.setActiveNumber = function(number) {
+      $rootScope.$broadcast('displayData', number);
+      console.log("Clicked number is: ", number)
+    };
+      
     $scope.clearActiveFactors = function() {
       activeFactorA = activeFactorB = null;
     };
