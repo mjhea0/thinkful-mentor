581e77b0ba359977994c7b032af772c971c3089b
Michael Herman <hermanmu@gmail.com>
2014-05-15 16:19:11 -0700

    doc tagger1

---
+++ document_tagger/doc_tagger_part1.py
@@ -0,0 +1,41 @@
+import re
+import sys
+
+from pg_sample_texts import DIV_COMM, MAG_CART
+
+documents = [DIV_COMM, MAG_CART]
+
+keywords = {}
+for keyword in sys.argv[1:]:
+    keywords[keyword] = re.compile(r'\b{}r\b'.format(re.IGNORECASE))
+
+title_search = re.compile(r'(title:\s*)(?P<title>[a-zA-Z, \n]* \s)', re.IGNORECASE)
+title_search = re.compile(r'(?:title:\s*)(?P<title>((\S*(\ )?)+)((\n(\ )+)(\S*(\ )?)*)*)',re.IGNORECASE | re.VERBOSE)
+author_search = re.compile(r'(author:*)(?P<author>.*)', re.IGNORECASE)
+translator_search = re.compile(r'(translator:*)(?P<translator>.*)', re.IGNORECASE)
+illustrator_search = re.compile(r'(illustrator:*)(?P<illustrator>.*)', re.IGNORECASE)
+
+for document_number, document in enumerate(documents):
+
+    title = re.search(title_search, document).group('title')
+    author = re.search(author_search, document)
+    translator = re.search(translator_search, document)
+    illustrator = re.search(illustrator_search, document)
+
+    if author:
+        author = author.group('author')
+    if translator:
+        translator = translator.group('translator')
+    if illustrator:
+        illustrator = illustrator.group('illustrator')
+
+    print "\nDocument #: {}".format(document_number+1)
+    print "--------------"
+    print "Title: {}".format(title)
+    print "Author(s): {}".format(author)
+    print "Translator(s): {}".format(translator)
+    print "Illustrator(s): {}".format(illustrator)
+    print "Keywords:"
+    for keyword in keywords:
+        keyword_count = len(re.findall(keyword, document))
+        print " - '{0}': {1}\n".format(keyword, keyword_count)
---
+++ document_tagger/pg_sample_texts.py
@@ -0,0 +1,86 @@
+DIV_COMM = """
+Project Gutenberg's The Divine Comedy, Complete, by Dante Alighieri
+ 
+This eBook is for the use of anyone anywhere at no cost and with
+almost no restrictions whatsoever.  You may copy it, give it away or
+re-use it under the terms of the Project Gutenberg License included
+with this eBook or online at www.gutenberg.org
+ 
+ 
+Title: The Divine Comedy, Complete
+       The Vision of Paradise, Purgatory and Hell
+ 
+Author: Dante Alighieri
+ 
+Illustrator: Gustave Dore
+ 
+Translator: Rev. H. F. Cary
+ 
+Release Date: September, 2005  [Etext #8800]
+Posting Date: June 11, 2009
+ 
+Language: English
+ 
+ 
+*** START OF THIS PROJECT GUTENBERG EBOOK THE DIVINE COMEDY, COMPLETE ***
+[...]
+His glory, by whose might all things are mov'd,
+Pierces the universe, and in one part
+[...]
+End of Project Gutenberg's The Divine Comedy, Complete, by Dante Alighieri
+*** END OF THIS PROJECT GUTENBERG EBOOK THE DIVINE COMEDY, COMPLETE ***
+***** This file should be named 8800.txt or 8800.zip *****
+This and all associated files of various formats will be found in:
+        http://www.gutenberg.org/8/8/0/8800/
+[...]
+"""
+ 
+MAG_CART = """
+Project Gutenberg's The Magna Carta
+ 
+Copyright laws are changing all over the world. Be sure to check the
+copyright laws for your country before downloading or redistributing
+this or any other Project Gutenberg eBook.
+ 
+This header should be the first thing seen when viewing this Project
+Gutenberg file.  Please do not remove it.  Do not change or edit the
+header without written permission.
+ 
+Please read the "legal small print," and other information about the
+eBook and Project Gutenberg at the bottom of this file.  Included is
+important information about your specific rights and restrictions in
+how the file may be used.  You can also find out about how to make a
+donation to Project Gutenberg, and how to get involved.
+ 
+ 
+**Welcome To The World of Free Plain Vanilla Electronic Texts**
+ 
+**eBooks Readable By Both Humans and By Computers, Since 1971**
+ 
+*****These eBooks Were Prepared By Thousands of Volunteers!*****
+ 
+ 
+Title: The Magna Carta
+ 
+Release Date: March, 2006 [eBook #10000]
+[Yes, we are more than one year ahead of schedule]
+[This file was first posted on October 15, 2003]
+[Date last updated: January 2, 2003]
+ 
+Edition: 0.1 [1.0 should be posted by December 10, 2003]
+[The date of the 10th Anniversay of eBook #100]
+ 
+Language: English
+ 
+ 
+*** START OF THE PROJECT GUTENBERG EBOOK THE MAGNA CARTA ***
+[...]
+JOHN, by the grace of God King of England, Lord of Ireland, Duke of
+Normandy and Aquitaine, and Count of Anjou, to his archbishops,
+bishops, abbots, earls, barons, justices, foresters, sheriffs,
+stewards, servants, and to all his officials and loyal subjects,
+Greeting. 
+[...]
+***
+*** END OF THE PROJECT GUTENBERG EBOOK THE MAGNA CARTA ***
+"""
