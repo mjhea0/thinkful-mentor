b7c8a64113d07265826837ba9e7f16a9a4087d90
Michael Herman <hermanmu@gmail.com>
2014-05-16 11:11:55 -0700

    mad libs p3

--- angular-curriculum/ngMadLibs/part3/js/main.js
+++ angular-curriculum/ngMadLibs/part3/js/main.js
@@ -4,7 +4,21 @@ $(function() {
 
 angular.module('ngMadlibs', [])
   .controller('madlibsController', function($scope) {
-    $scope.add = function() {
-      console.log($scope.name)
-    };
+    $scope.submitForm = function() {
+      // check to make sure the form is valid
+      if ($scope.madForm.$valid) {
+        if ($scope.madForm.gender.$modelValue === "Male") {
+          $('#reset-btn').show()
+          $('#male-text').show()
+          $('#female-text').hide()
+        } else if ($scope.madForm.gender.$modelValue === "Female") {
+          $('#reset-btn').show()
+          $('#female-text').show()
+          $('#male-text').hide()
+        }
+      }
+    }
+    $scope.resetForm = function() {
+      console.log("almost done!")
+    }
   });
