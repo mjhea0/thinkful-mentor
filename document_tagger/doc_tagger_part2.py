f1c3927b78554ab05a3122040d20aa8b0da2e4de
Michael Herman <hermanmu@gmail.com>
2014-05-22 10:51:20 -0600

    updated doc_tagger part2

--- document_tagger/doc_tagger_part2.py
+++ document_tagger/doc_tagger_part2.py
@@ -2,20 +2,21 @@ import os
 import sys
 import re
 
-# path to the directory housing text docs as command line argument
+# path to the directory housing text docs specified via command line argument
 directory = sys.argv[1]
 
+# generate regex pattern for keywords specified via command line arguments
+keywords = {}
+for keyword in sys.argv[2:]:
+    keywords[keyword] = re.compile(r'\b '+ keyword + r'\b', re.IGNORECASE)
+
+# you could also use a dict comprehension; less code, but harder to read
 # # create dict of keywords with a dict comprehension
 # keywords = {
 #     keyword: re.compile(r'\b' + keyword + r'\b') for keyword in sys.argv[2:]
 # }
 
-
-keywords = {}
-for keyword in sys.argv[2:]:
-    keywords[keyword] = keyword
-
-# regular expressions
+# regex patterns for title, author, illustrator and translator
 title_search = re.compile(
     r'(?:title:\s*)(?P<title>((\S*( )?)+)' +
     r'((\n(\ )+)(\S*(\ )?)*)*)',
@@ -34,13 +35,17 @@ illustrator_search = re.compile(
     re.IGNORECASE
 )
 
+# find and iterate through all the files in the directory
 for file_name in (os.listdir(directory)):
+    # only look at files ending in '.txt'
     if file_name.endswith('.txt'):
-        file_name_path = os.path.join(directory, file_name)
+        # get the full path of the file name by adding the directory path
+        file_name_path = os.path.join(directory, file_name) 
         # search individual files
         with open(file_name_path, 'r') as f:
             full_text = f.read()
-        # search individual file for text that matches regex
+
+        # search individual files for text that match the regular expressions
         title = re.search(title_search, full_text)
         if title:
             title = title.group('title')
@@ -53,16 +58,18 @@ for file_name in (os.listdir(directory)):
         illustrator = re.search(illustrator_search, full_text)
         if illustrator:
             illustrator = illustrator.group('illustrator')
-        # outputs
+
+        # output the results
         print "---" * 25
         print "Here's the info for {}:".format(file_name)
         print "Title: {}".format(title)
         print "Author(s): {}".format(author)
         print "Translator(s): {}".format(translator)
         print "Illustrator(s): {}".format(illustrator)
+
+        # count # of times each keyword appeara in the searched texts and output the results
         print "\n# ---- KEYWORD REPORT ---- #\n\n"
         for keyword in keywords:
-            print keyword
             print "'{0}': {1}".format(
                 keyword,
                 len(re.findall(keywords[keyword], full_text))
