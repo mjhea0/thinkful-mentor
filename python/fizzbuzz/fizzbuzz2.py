12f3e42babeab5c4a3efe3dc545c75f2c878ad35
Michael Herman <hermanmu@gmail.com>
2014-06-03 15:45:18 -0500

    fizzbuzz v2

--- python/fizzbuzz/fizzbuzz2.py
+++ python/fizzbuzz/fizzbuzz2.py
@@ -1,11 +1,23 @@
 import sys
 
+# get value
 if sys.argv[1:]:
     upper_limit = sys.argv[1]
 else:
     upper_limit = raw_input("Enter the upper limit of the range: ")
 
-for i in xrange(1, int(upper_limit)+1):
+# convert to int / handle exceptions
+while True:
+    try:
+        value = int(upper_limit)
+        break
+    except ValueError:
+        upper_limit = raw_input(
+            "Incorect data type. Please enter an 'integer' for the upper limit of the range: "
+        )
+        
+# run fizzbuzz
+for i in xrange(1, value + 1):
     if i % 15 == 0:
         print "FizzBuzz"
     elif i % 3 == 0:
