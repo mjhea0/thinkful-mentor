9eafd6ff30c879117527887fd993567d3d50d462
Michael Herman <hermanmu@gmail.com>
2014-08-30 18:46:48 -0700

    added error handler and big pictures to flask-yummly

--- python/flask-yummly/yummly/api.py
+++ python/flask-yummly/yummly/api.py
@@ -6,6 +6,7 @@ from secret import APP_ID, APP_KEY
 
 
 def get_ingredients(ingredient):
+
     res = requests.get(
         "http://api.yummly.com/v1/api/recipes",
         params={'_app_id': APP_ID, '_app_key': APP_KEY, 'q': ingredient}
--- python/flask-yummly/yummly/views.py
+++ python/flask-yummly/yummly/views.py
@@ -7,26 +7,46 @@ import json
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    recipe_response = []
-    ingredients = []
 
     if request.method == "POST":
+
+        recipe_response = []
+        ingredients = []
+        errors = []
+
         ingredient = request.form["ingredient"]  # grab ingredient from the form
-        response = api.get_ingredients(ingredient)  # make api call
-        response_to_json = json.loads(response)  # convert to json from unicode
 
-        matches = response_to_json["matches"]
-        for match in matches:
-            recipe_response.extend([
-                match["recipeName"], match["smallImageUrls"][0], match["id"]
-            ])
-            ingredients.append(match["ingredients"])
-            break  # just loop once for now, grabbing the first recipe
+        try:
+            response = api.get_ingredients(ingredient)  # make api call
+            response_to_json = json.loads(response)  # convert to json from unicode
 
-        ingredients = ingredients[0]
+            matches = response_to_json["matches"]
 
-    return render_template(
-        "index.html",
-        recipe_response=recipe_response,
-        ingredients=ingredients
-    )
+            for match in matches:  # loop through json results
+
+                image = match['imageUrlsBySize']['90'].replace('s90-c', 's230-c')
+
+                recipe_response.extend([
+                    match["recipeName"], image, match["id"]
+                ])  # grab certain results
+                ingredients.append(match["ingredients"])
+                break  # just loop once for now, grabbing the first recipe
+
+            ingredients = ingredients[0]
+
+        except:  # silencing all errors - bad!
+
+            errors.append("Something went wrong!")
+
+        return render_template(
+            "index.html",
+            recipe_response=recipe_response,
+            ingredients=ingredients,
+            errors=errors
+        )
+
+    else:
+
+        return render_template(
+            "index.html"
+        )
