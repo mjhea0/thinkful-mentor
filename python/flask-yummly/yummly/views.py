feb84b709bde67c1503629e9e1d96e2409ff2dd3
Michael Herman <hermanmu@gmail.com>
2014-09-02 19:14:23 -0700

    updated api

--- python/flask-yummly/yummly/views.py
+++ python/flask-yummly/yummly/views.py
@@ -9,8 +9,6 @@ def index():
 
     if request.method == "POST":
 
-        recipe_response = []
-        ingredients = []
         errors = []
 
         ingredient = request.form["ingredient"]  # grab ingredient from the form
@@ -18,17 +16,17 @@ def index():
         try:
             response = api.get_ingredients(ingredient)  # make api call
 
-            for match in response["matches"]:  # loop through json results
+            # for match in response["matches"]:  # loop through json results
 
-                image = match['imageUrlsBySize']['90'].replace('s90-c', 's230-c')  # update url
+            #     image = match['imageUrlsBySize']['90'].replace('s90-c', 's230-c')  # update url
 
-                recipe_response.extend([
-                    match["recipeName"], image, match["id"]
-                ])  # grab reciepe name, image, and id
-                ingredients.append(match["ingredients"])
-                break  # just loop once for now, grabbing the first recipe
+            #     recipe_response.extend([
+            #         match["recipeName"], image, match["id"]
+            #     ])  # grab reciepe name, image, and id
+            #     ingredients.append(match["ingredients"])
+            #     break  # just loop once for now, grabbing the first recipe
 
-            ingredients = ingredients[0]
+            # ingredients = ingredients[0]
 
         except:  # silencing all errors - bad!
 
