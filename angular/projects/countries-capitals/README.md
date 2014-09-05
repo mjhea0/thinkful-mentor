a08be4469a17348f2f7d5909ab8a3a1b826c7e10
Michael Herman <hermanmu@gmail.com>
2014-09-04 23:37:54 -0700

    added some basic unit test

--- angular/projects/countries-capitals/README.md
+++ angular/projects/countries-capitals/README.md
@@ -3,3 +3,4 @@
 1. `npm install`
 2. `bower install`
 3. `gulp` or `gulp build`
+4. `karma start`
--- angular/projects/countries-capitals/test/unit/appSpec.js
+++ angular/projects/countries-capitals/test/unit/appSpec.js
@@ -1,1 +1,38 @@
-//
+describe("My First Test", function(){
+
+    it("should be true", function(){
+        expect(true).toBe(true);
+    });
+});
+
+
+describe('Routes!', function(){
+  beforeEach(module("CountriesCapitalsApp"));
+
+  it('should load home template', function(){
+    inject(function($route, $rootScope, $location, $httpBackend){
+      var route = $route.routes['/'];
+      $httpBackend.whenGET(route.templateUrl).respond('...');
+
+      $rootScope.$apply(function() {
+        $location.path(route.originalPath);
+      });
+
+      expect($route.current.templateUrl).toBe("./home/home.html");
+    });
+  });
+
+  it('should load countries template', function(){
+    inject(function($route, $rootScope, $location, $httpBackend){
+      var route = $route.routes['/countries'];
+      $httpBackend.whenGET(route.templateUrl).respond('...');
+
+      $rootScope.$apply(function() {
+        $location.path(route.originalPath);
+      });
+
+      expect($route.current.templateUrl).toBe("./countries/countries.html");
+    });
+  });
+
+});
