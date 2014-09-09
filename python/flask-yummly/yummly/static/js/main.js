4d018947658d61f955ff61e596c50cb7bf1b0119
Michael Herman <hermanmu@gmail.com>
2014-09-09 18:11:15 -0500

    updates

--- python/flask-yummly/yummly/static/js/main.js
+++ python/flask-yummly/yummly/static/js/main.js
@@ -1,3 +1,19 @@
 $(function() {
-  console.log("dom is ready!");
+    console.log("dom is ready!");
+   //  $('#post-form').on('submit', function(event){
+   //      event.PreventDefault;
+   //      console.log("yay!")
+   //      $('#top').hide()
+   //      $.ajax({
+   //          type: "POST",
+   //          url: "/",
+   //          data : { ingredient_list : $('.ingredients').val() },
+   //          success: function(result) {
+   //              console.log(result);
+   //          },
+   //          error: function() {
+   //              console.log("yikes")
+   //          }
+   //      });
+   // });
 });
