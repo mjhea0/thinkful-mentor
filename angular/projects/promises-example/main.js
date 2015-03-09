4141063d36d99ecd6f30591803a321f81ca8fa62
Michael Herman <hermanmu@gmail.com>
2015-03-09 08:30:13 -0600

    updated promises

--- angular/projects/promises-example/main.js
+++ angular/projects/promises-example/main.js
@@ -2,17 +2,17 @@ var app = angular.module('myApp', []);
 
 app.controller('appController', function($scope, $q, $timeout) {
 
-  $scope.mydata = "Hello, "
+  $scope.mydata = "Hello, ";
 
   var defer = $q.defer();
 
   defer.promise
     .then(function(val) {
-      $scope.mydata += val
-      return val
-    })
+      $scope.mydata += val;
+      // return val;
+    });
 
   $timeout(function() {
     defer.resolve("World!");
-  },3000)
+  },3000);
 });
