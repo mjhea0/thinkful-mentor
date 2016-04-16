1d94031d31a218c08903f654bacaf2ed1b79e1a7
Michael Herman <hermanmu@gmail.com>
2016-04-16 11:23:56 -0600

    v1

--- angular/projects/angular-shopping-cart/src/app/components/item-list/item-list.directive.js
+++
@@ -1,6 +0,0 @@
-app.directive('itemList', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'app/components/item-list/item-list.view.html'
-  }
-})
--- angular/projects/angular-shopping-cart/src/app/components/nav-bar/nav-bar.directive.js
+++
@@ -1,7 +0,0 @@
-app.directive('navBar', function() {
-  return {
-    restrict: 'E',
-    scope: {},
-    templateUrl: 'app/components/nav-bar/nav-bar.view.html'
-  };
-});
